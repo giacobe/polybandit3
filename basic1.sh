31bf5ace362cc54461b8c9eba69d5da59c164cb0
Nick Giacobe <nxg13@ist.psu.edu>
2022-06-18 00:46:01 -0400

    Update basic1.sh

--- basic1.sh
+++ basic1.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+basic1Password = "basic1password"
+export BASIC1_HASH=$(echo -n "$USER_ID"+currentDate+newPass+basic1password | md5sum)
 
 ## create static directories
 cd $userName
 mkdir "basic1"
-
+cat $BASIC1_HASH > basic1/inhere.txt
 
 echo "*"
 echo "*"
