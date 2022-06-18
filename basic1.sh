090f27b9135146d9076c74eeff8ab62fe16835fc
Nick Giacobe <nxg13@ist.psu.edu>
2022-06-18 00:38:30 -0400

    Create basic1.sh

---
+++ basic1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## create static directories
+cd $userName
+mkdir "basic1"
+
+
+echo "*"
+echo "*"
+echo "*"
+echo "Please change to the 'basic1' directory and display contents of the one file you find there." > basic1/README
+echo "This will be the password for this level " >> basic1/README
