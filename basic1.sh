f99402839aea65a6836aaea8e810370a97fb147e
Nick Giacobe <nxg13@ist.psu.edu>
2022-06-18 11:17:07 -0400

    Update basic1.sh

--- basic1.sh
+++ basic1.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+#set and confirm inputs
 basic1Password="basic1password"
 echo $USER_ID
 echo $currentDate
 echo $newPass
 echo $basic1Password
 
-export BASIC1_HASH=$(echo -n "$USER_ID"+"$currentDate"+"$newPass"+"$basic1Password" | md5sum)
+export BASIC1_HASH=$(echo -n "$USER_ID"+"$currentDate"+"$newPass"+"$basic1Password" | md5sum | grep -o '^\S\+')
 
 ## create static directories
 echo $userName
 cd /home/$userName
 mkdir "basic1"
-cat $BASIC1_HASH > basic1/inhere.txt
+echo $BASIC1_HASH > basic1/inhere.txt
 
-echo "*"
-echo "*"
-echo "*"
-echo "Please change to the 'basic1' directory and display contents of the one file you find there." > basic1/README
-echo "This will be the password for this level " >> basic1/README
+echo "*" > basic1/README.txt
+echo "*" >> basic1/README.txt
+echo "*" >> basic1/README.txt
+echo "Display contents of the one file you find in this directory." >> basic1/README.txt
+echo "The contents will be the password for this level." >> basic1/README.txt
