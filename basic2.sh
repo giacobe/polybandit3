e55dbc225296f577cbb201bd3d26ffb660c1a5be
Nick Giacobe <nxg13@ist.psu.edu>
2022-06-18 12:30:49 -0400

    Create basic2.sh

---
+++ basic2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#set and confirm inputs
+levelPassword="basic2password"
+levelToBuild="basic2"
+readMeLocation=$levelToBuild"/README.txt"
+
+level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | grep -o '^\S\+')
+
+## create static directories
+echo $userName
+cd /home/$userName
+mkdir $levelToBuild
+echo $level_HASH | base64 | cut -c 1-8 > $levelToBuild/inhere.txt
+
+echo "*" > $readMeLocation
+echo "*" >> $readMeLocation
+echo "*" >> $readMeLocation
+echo "Display contents of the inhere.txt file in this directory." >> $readMeLocation
+echo "The contents will be the password for this level." >>$readMeLocation
