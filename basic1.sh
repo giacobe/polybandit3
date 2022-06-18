74cb0dae53a5d8f963bddb56ef0021dece36b2d6
Nick Giacobe <nxg13@ist.psu.edu>
2022-06-18 12:01:20 -0400

    Update basic1.sh

--- basic1.sh
+++ basic1.sh
@@ -3,22 +3,20 @@
 #set and confirm inputs
 basic1Password="basic1password"
 levelToBuild="basic1"
-readMeLocation=$levelToBuild+"/README.txt"
+readMeLocation=$levelToBuild"/README.txt"
 echo $USER_ID >> vars.txt
 echo $currentDate >> vars.txt
 echo $newPass >> vars.txt
 echo $basic1Password >> vars.txt
 #echo $levelToBuild >> vars.txt
 
-export BASIC1_HASH=$(echo -n "$USER_ID"+"$currentDate"+"$newPass"+"$basic1Password" | md5sum | grep -o '^\S\+')
+export BASIC1_HASH=$(echo -n "$USER_ID$currentDate$newPass$basic1Password" | md5sum | grep -o '^\S\+')
 
 ## create static directories
 echo $userName
 cd /home/$userName
 mkdir $levelToBuild
 echo $BASIC1_HASH | base64 | cut -c 1-8 > $levelToBuild/inhere.txt
-
-readMeLocation=$levelToBuild"/README.txt"
 
 echo "*" > $readMeLocation
 echo "*" >> $readMeLocation
