e8051cc8f1301136635ed41004122bd46504897f
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-08-08 09:26:08 -0400

    Create TJInstall.sh

---
+++ TJInstall.sh
@@ -0,0 +1,173 @@
+#!/bin/sh
+
+# Eventually, we want this script to autorun from the /root user and prevent the participant from breaking out of it with <ctrl> C
+#trap ' ' 2 20
+
+# This sets the date/time correctly from the hosty system's date/time
+#rc-update delete hwclock boot
+#rc-service hwclock restart
+#date +"%Y-%m-%d" > currentDate.txt
+export currentDate=$(date +"%Y-%m-%d" | head -n 1)
+
+# Read the username from the keyboard.
+# eventually, we need to do some validation that this at least looks like an email address
+export USER_ID=""
+echo "Enter your PSU email (xyz1234@psu.edu): "
+read USER_ID
+
+# why exactly are we doing this?!?
+#echo -n "$USER_ID" > userID.txt
+
+export USER_HASH=$(echo -n "$USER_ID" | md5sum)
+echo -n "$USER_HASH" > userHash.txt
+
+#set up the limited user account on the linux system that the end-user will be playing the game from
+# this sets up the user account for the game
+export userName="polylinuxgame"
+export newPass="Password1"
+#useradd -p $newPass -m $userName #(wrong syntax for Buildroot/ash)
+#set up the directories that are missing
+mkdir /home
+mkdir /home/$userName
+adduser -h /home/$username -D -g "User" $userName
+chown $userName:$userName /home/$userName
+
+export origInstallDir=$(pwd)
+
+# Call each level's install script
+# remove each level's install script since we don't need it anymore
+
+echo building TJlevel1
+./TJlevel1.sh
+cd $origInstallDir
+rm TJlevel1.sh
+
+echo building TJlevel2
+./TJlevel2.sh
+cd $origInstallDir
+rm TJlevel2.sh
+
+echo building TJlevel3
+./TJlevel3.sh
+cd $origInstallDir
+rm TJlevel3.sh
+
+echo building TJlevel4
+./TJlevel4.sh
+cd $origInstallDir
+rm TJlevel4.sh
+
+echo building TJlevel5
+./TJlevel5.sh
+cd $origInstallDir
+rm TJlevel5.sh
+
+echo building TJlevel6
+./TJlevel6.sh
+cd $origInstallDir
+rm TJlevel6.sh
+
+echo building TJlevel7
+./TJlevel7.sh
+cd $origInstallDir
+rm TJlevel7.sh
+
+
+#--------------------------------------------- Mover
+
+mv TJlevel1 /home/polylinuxgame
+mv TJlevel2 /home/polylinuxgame
+mv TJlevel3 /home/polylinuxgame
+mv TJlevel4 /home/polylinuxgame
+mv TJlevel5 /home/polylinuxgame
+mv TJlevel6 /home/polylinuxgame
+mv TJlevel7 /home/polylinuxgame
+
+#---------------------------------------------
+
+#echo building basic5
+#./basic5.sh
+#cd $origInstallDir
+#rm basic5.sh
+
+#echo building basic6
+#./basic6.sh
+#cd $origInstallDir
+#rm basic6.sh
+
+#echo building basic7
+#./basic7.sh
+#cd $origInstallDir
+#rm basic7.sh
+
+#echo building basic8
+#./basic8.sh
+#cd $origInstallDir
+#rm basic8.sh
+
+#echo building basic9
+#./basic9.sh
+#cd $origInstallDir
+#rm basic9.sh
+
+#echo building basic10
+#./basic10.sh
+#cd $origInstallDir
+#rm basic10.sh
+
+#echo building level1
+#./level1.sh
+#cd $origInstallDir
+#rm level1.sh
+
+#echo building level2
+#./level2.sh
+#cd $origInstallDir
+#rm level2.sh
+
+#echo building level3
+#./level3.sh
+#cd $origInstallDir
+#rm level3.sh
+
+#echo building level4
+#./level4.sh
+#cd $origInstallDir
+#rm level4.sh
+
+#clean up after ourselves
+#rm -rf dictionaries
+#rm README.md
+#rm userHash.txt
+
+# why?  Oh, I guess we originally generated this all in the /root directory. Fixed this later by doing a CD to the correct directory in each level's .sh
+#cp -r /root/PolyLinuxGame/* /home/$userName/
+
+# why are we removing these?
+#rm /home/polylinuxgame/level1Verify.sh
+#rm /home/polylinuxgame/level2Verify.sh
+#rm /home/polylinuxgame/level3Verify.sh
+#rm /home/polylinuxgame/level4Verify.sh
+#rm -rf /root/PolyLinuxGame
+#cp -r /root/PolyLinuxGame/dictionaries /home/$userName/
+
+# clean up the ownership the files in the gameplayer's homedir. might need to add a group as well
+chown -R $userName /home/$userName
+
+#clear
+
+#rm setup.sh
+echo "Done!" 
+echo "***************************************"
+echo "*   Welcome to The PolyLinux Game     *"
+echo "*  Change to different directories    *"
+echo "*  and use 'cat README.txt' to read   *"
+echo "*   the instructions for the level    *"
+echo "*             Good Luck!              *"
+echo "*    You created this session on:     *"
+echo "*             $currentDate              *"
+echo "***************************************"
+
+#cd /home/$userName
+su -l $userName
+#sleep 10
