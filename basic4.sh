02f4d4d5fa644d3c822cc555065f6676daa0baea
Nick Giacobe <nxg13@ist.psu.edu>
2022-12-21 10:19:26 -0500

    Update basic4.sh

--- basic4.sh
+++ basic4.sh
@@ -2,7 +2,7 @@
 
 #set and confirm inputs
 levelPassword="basic4password"
-levelToBuild="basic4"
+#levelToBuild="basic4"
 readMeLocation=$levelToBuild"/README.txt"
 
 level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | sha256sum | grep -o '^\S\+')
