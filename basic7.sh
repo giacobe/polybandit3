b074154245a5d63993346c368c80740d931a5b0c
Nick Giacobe <nxg13@ist.psu.edu>
2022-12-21 00:26:00 -0500

    Update basic7.sh

--- basic7.sh
+++ basic7.sh
@@ -5,7 +5,7 @@ levelPassword="basic7password"
 levelToBuild="basic7"
 readMeLocation=$levelToBuild"/README.txt"
 
-level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | grep -o '^\S\+')
+level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | sha256sum | grep -o '^\S\+')
 
 ## create static directories
 cd /home
