27e1f80e6a0bf77ea9b5762e2d1d1339ee6672fb
Nick Giacobe <nxg13@ist.psu.edu>
2022-12-21 11:02:18 -0500

    Update basic2.sh

--- basic2.sh
+++ basic2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #set and confirm inputs
-levelPassword="basic2password"
+#levelPassword="basic2password"
 #levelToBuild="basic2"
-readMeLocation=$levelToBuild"/README.txt"
+#readMeLocation=$levelToBuild"/README.txt"
 
-level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | sha256sum | grep -o '^\S\+')
+#level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | sha256sum | grep -o '^\S\+')
 
 ## create static directories
 #echo $userName
